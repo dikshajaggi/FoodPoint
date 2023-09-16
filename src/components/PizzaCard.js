293b9b162a479be9d70a61a4fd124f9c6bd7191d
developer 04 <diksha2000may@gmail.com>
2023-09-16 15:27:03 +0530

    issue fixed

--- src/components/PizzaCard.js
+++ src/components/PizzaCard.js
@@ -3,7 +3,7 @@ import "../style.css"
 import { CardWrapper, InfoWrapper, RestInfo, RestName } from './styledComponents/PizzaCard'
 
 const Card = (props) => {
-    const { name, costForTwoString, cuisines, sla, cloudinaryImageId, avgRating, aggregatedDiscountInfo } = props
+    const { name, costForTwoString, sla, cloudinaryImageId, avgRating, aggregatedDiscountInfo } = props
     return (
         <CardWrapper>
             <img className="pizzaImg" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + cloudinaryImageId} alt="" />
