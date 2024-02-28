f7e9cf107a212fac841ffe7b645fb3901766ce06
cee-diksha <diksha2000may@gmail.com>
2024-02-28 20:37:03 +0530

    offers page fixed

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 import Card from "../components/RestCard"
 import Header from "../components/Header"
 import RestCard from "../shimmerUI/RestCard"
-import { data } from "../assets/data"
-import {OfferBanner, OfferWrapper } from "./styledComponents/Offers"
+import data from "../assets/data.json"
+import { OfferBanner, OfferWrapper } from "./styledComponents/Offers"
 import Footer from "../components/Footer"
 import { CardWrapper } from "../layouts/styledComponents/Main"
 
@@ -22,34 +22,33 @@ const Offers = () => {
     useEffect(() => {
         setTimeout(() => {
             setIsLoading(false)
-            getData()
+            // getData()
+            setOffers(data?.cards?.card.card.gridElements.infoWithStyle.restaurants)
         }, 1000)
 
     }, [])
 
-    async function getData() {
-        await axios.get("https://www.swiggy.com/api/seo/getListing?lat=28.65420&lng=77.23730").then(item => {
-            console.log(item.data.data.success.cards[1].card.card.gridElements.infoWithStyle.restaurants, "checking item")
-            setOffers(item.data.data.success.cards[1].card.card.gridElements.infoWithStyle.restaurants)
-        })
+    // async function getData() {
+    //     await axios.get("https://www.swiggy.com/api/seo/getListing?lat=28.65420&lng=77.23730").then(item => {
+    //         console.log(item.data.data.success.cards[1].card.card.gridElements.infoWithStyle.restaurants, "checking item")
+    //         setOffers(item.data.data.success.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    //     })
 
-        await axios.get("https://www.swiggy.com/dapi/offers/payment?lat=28.7040592&lng=77.10249019999999&offset=0").then(item => {
-            console.log(item.data.data.cards, "payment")
-            setPaymentOffers(item.data.data.cards)
-        })
-    }
+    //     await axios.get("https://www.swiggy.com/dapi/offers/payment?lat=28.7040592&lng=77.10249019999999&offset=0").then(item => {
+    //         console.log(item.data.data.cards, "payment")
+    //         setPaymentOffers(item.data.data.cards)
+    //     })
+    // }
     return (
         <>
             <Header />
             <OfferWrapper>
                 <OfferBanner> Explore top deals and offers exclusively for you! </OfferBanner>
                 <CardWrapper>
-                            {isLoading ? data?.map((item) => {
-                            return <RestCard />
-                            }) : offers?.slice(1)?.map((item) => {
-                            return <Link to={`/rest/${item.info.feeDetails?.restaurantId}`} style={{ textDecoration: 'none' }} >< Card {...item.info} /></Link>
-                            })}
-                        </CardWrapper>
+                    {offers?.slice(1)?.map((item) => {
+                        return <Link to={`/rest/${item.info.feeDetails?.restaurantId}`} style={{ textDecoration: 'none' }} >< Card {...item.info} /></Link>
+                    })}
+                </CardWrapper>
             </OfferWrapper>
             <Footer />
         </>
