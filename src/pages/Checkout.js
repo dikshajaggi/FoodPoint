29da13e151586958115eebde802b7395b4e7e29d
cee-diksha <cee.diksha@gmail.com>
2024-03-02 10:25:41 +0530

    total price functionality for cart done

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,33 +5,41 @@ import { Context } from '../utilities/context/Context'
 import { LinkStyled } from '../components/styledComponents/Header'
 import _ from 'lodash'
 import { CheckoutBtn, CheckoutLabel, CheckoutWrapper, TotalLabels, TotalWrapper } from './styledComponents/Checkout'
+import { useSelector } from 'react-redux'
 
 const Checkout = () => {
     const username = useContext(UserContext)
-    const context = useContext(Context)
-    const [items, setItems] = useState(context.quantity.map(item => item.qty))
-    const [totalItems, setTotalItems] = useState(_.sum(items))
-    const [price, setPrice] = useState(context.quantity.map(item => item.price))
-    const [totalPrice, setTotalPrice] = useState(_.sum(price))
+    // const context = useContext(Context)
+    const cartItems = useSelector(state => state.cart.items)
+    const [total, setTotal] = useState()
+
+    // const [items, setItems] = useState(context.quantity.map(item => item.qty))
+    // const [totalItems, setTotalItems] = useState(_.sum(items))
+    // const [price, setPrice] = useState(context.quantity.map(item => item.price))
+    // const [totalPrice, setTotalPrice] = useState(_.sum(price))
+
+    // useEffect(() => {
+    //     const price_current = context.quantity.map(item => item.price)
+    //     const items_current = context.quantity.map(item => item.qty)
+    //     setItems(context.quantity.map(item => item.qty))
+    //     setTotalItems(_.sum(items_current))
+    //     setPrice(context.quantity.map(item => item.price))
+    //     setTotalPrice(_.sum(price_current))
+    //     context.setTotalItems(_.sum(items_current))
+    //     context.setTotalPrice(_.sum(price_current))
+    // }, [context.qtyCheck])
 
     useEffect(() => {
-        const price_current = context.quantity.map(item => item.price)
-        const items_current = context.quantity.map(item => item.qty)
-        setItems(context.quantity.map(item => item.qty))
-        setTotalItems(_.sum(items_current))
-        setPrice(context.quantity.map(item => item.price))
-        setTotalPrice(_.sum(price_current))
-        context.setTotalItems(_.sum(items_current))
-        context.setTotalPrice(_.sum(price_current))
-    }, [context.qtyCheck])
+       setTotal(cartItems.reduce((acc, current) => acc + current.price , 0))
+    }, [cartItems])
 
     return (
         <CheckoutWrapper>
             <CheckoutLabel>Checkout</CheckoutLabel>
             <br></br>
             <TotalWrapper>
-                <TotalLabels>Total Items : <span style={{ fontWeight: 400 }}>{totalItems}</span></TotalLabels>
-                <TotalLabels>Total Price : <span style={{ fontWeight: 400 }}>{totalPrice}</span></TotalLabels>
+                {/* <TotalLabels>Total Items : <span style={{ fontWeight: 400 }}>{totalItems}</span></TotalLabels> */}
+                <TotalLabels>Total Price : <span style={{ fontWeight: 400 }}>Rs. {(total) / 100}</span></TotalLabels>
             </TotalWrapper>
             {username.user !== "" ? <LinkStyled to="/payment"><CheckoutBtn>Checkout</CheckoutBtn></LinkStyled> : <LinkStyled to="/login"><CheckoutBtn>Checkout</CheckoutBtn></LinkStyled>}
         </CheckoutWrapper>
