84fa49f658f62371f04ec50a2dcf0d032d566e5c
cee-diksha <cee.diksha@gmail.com>
2024-08-27 16:35:44 +0530

    warning removed

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from 'react'
 import api from '../utilities/api'
 import { UserContext } from '../utilities/context/UserContext'
